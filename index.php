a00919419468a6a381fbf77a1edb50c27b84cdc1
mickael <nv.osselaer@gmail.com>
2021-01-29 13:13:14 +0100

    exercice 158

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
+
+if(isset($_POST['nombreX']) && isset($_POST['nombreY'])) {
+    $x = intval($_POST['nombreX']);
+    $y = intval($_POST['nombreY']);
+    if($x < 0 || $x > 100) {
+        $x = 0 ;
+    }
+    if($y < 0 || $y > 100) {
+        $y = 0;
+    }
+
+    echo rand($x, $y);
+}
+
+
+
+
 /*
 - Créer un script php qui permet de générer un nombre aléatoire compris entre x et y
 - Coté front, créer un formulaire acceptant deux valeurs : x et y
